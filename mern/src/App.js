df64a679ab6585c6b4b0119ae96266e35cbd93a3
Ihatnest <85156361+Ihatnest@users.noreply.github.com>
2022-07-16 10:41:26 +0700

    done

--- mern/src/App.js
+++ mern/src/App.js
@@ -1,26 +1,38 @@
-import React, { Component } from 'react';
+import React,{ Component } from 'react';
 import './App.css';
 import ApiGET from './component/5apiGET/apiGET';
+import axios from 'axios'
 class App extends Component {
   state = {
     data: []
   }
 
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(json => {
-      this.setState({
-        data: json
+  componentDidMount() {
+    // menggunakan fetch
+    // fetch('https://jsonplaceholder.typicode.com/posts')
+    // .then(response => response.json())
+    // .then(json => {
+    //   this.setState({
+    //     data: json
+    //   })
+    // })
+
+    // menggunakan axios
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then((data) => {
+        this.setState({
+          data: data.data
+        })
       })
-    })
   }
 
-  render(){
+
+
+  render() {
     return (
       <div className="App">
         {
-          this.state.data.map( data => {
+          this.state.data.map(data => {
             return <ApiGET key={data.id} title={data.title} desc={data.body} />
           })
         }
